eed552a0d9dd3dfe402b25698f12e8e0b6c82bfe
Joseph Tatum <jtatum@newrelic.com>
2023-12-01 17:44:47 -0800

    upgrade to Kirby v4

--- site/plugins/kirby-markdown-field/src/components/Buttons/Link.js
+++ site/plugins/kirby-markdown-field/src/components/Buttons/Link.js
@@ -1,88 +1,175 @@
 import Button from "./Button.js";
 
 export default class Link extends Button {
-  get button() {
-    return {
-      icon: "url",
-      label: this.input.$t("toolbar.button.link") + this.formatKeyName(this.keys()[0]),
-      command: () => this.editor.emit("dialog", this),
-    };
-  }
+	get button() {
+		return {
+			icon: "url",
+			label:
+				this.input.$t("toolbar.button.link") +
+				this.formatKeyName(this.keys()[0]),
+			command: () => this.openDialog()
+		};
+	}
 
-  get command() {
-    return (value) => {
-      if (this.isDisabled()) {
-        return;
-      }
+	openDialog() {
+		const fields = {
+			href: {
+				label: window.panel.$t("link"),
+				type: "link",
+				placeholder: window.panel.$t("url.placeholder"),
+				icon: "url"
+			},
+			text: {
+				label: window.panel.$t("link.text"),
+				type: "text"
+			}
+		};
 
-      if (value.type === "email") {
-        const email = value.email !== null ? value.email : "";
+		if (this.useKirbytext) {
+			fields["target"] = {
+				label: window.panel.$t("open.newWindow"),
+				type: "toggle",
+				text: [window.panel.$t("no"), window.panel.$t("yes")]
+			};
+		}
 
-        if (this.useKirbytext) {
-          const text = value.text ? ` text: ${value.text}` : "";
-          this.editor.insert(`(email: ${email}${text})`);
-        } else {
-          if (value.text) {
-            this.editor.insert(`[${value.text}](mailto:${email})`);
-          } else {
-            this.editor.insert(`<${email}>`);
-          }
-        }
-      } else {
-        const url = value.url !== null ? value.url : "";
+		this.input.$panel.dialog.open({
+			component: "k-link-dialog",
+			props: {
+				fields: {
+					href: {
+						label: window.panel.$t("link"),
+						type: "link",
+						placeholder: window.panel.$t("url.placeholder"),
+						icon: "url"
+					},
+					text: {
+						label: window.panel.$t("link.text"),
+						type: "text"
+					},
+					target: {
+						label: window.panel.$t("open.newWindow"),
+						type: "toggle",
+						text: [window.panel.$t("no"), window.panel.$t("yes")]
+					}
+				},
+				value: ""
+			},
+			on: {
+				cancel: () => this.input.focus(),
+				submit: (values) => {
+					this.input.$panel.dialog.close();
+					delete values.title;
+					values.text = values.text || null;
+					this.insertLink(values);
+				}
+			}
+		});
+	}
 
-        if (this.useKirbytext) {
-          const text = value.text ? ` text: ${value.text}` : "";
-          const blank = value.blank ? " target: _blank" : "";
-          this.editor.insert(`(link: ${url}${text}${blank})`);
-        } else {
-          if (value.text) {
-            this.editor.insert(`[${value.text}](${url})`);
-          } else {
-            this.editor.insert(`<${url}>`);
-          }
-        }
-      }
-    }
-  }
+	insertLink({ href, text, target }) {
+		if (this.isDisabled()) {
+			return;
+		}
 
-  configure(options) {
-    if (typeof options === "string") {
-      options = { style: options };
-    }
+		if (href === "" || href === null) {
+			return;
+		}
 
-    Button.prototype.configure.call(this, options);
+		const hasText = text !== "" && text !== null;
+		const linkType = this.linkType(href);
 
-    if (!["markdown", "kirbytext", null].includes(this.options.style)) {
-      throw "Link style must be either `markdown`, `kirbytext` or `null`.";
-    }
-  }
+		if (linkType === "email") {
+			const email = href.replace(/^email:/, "");
 
-  get defaults() {
-    return {
-      blank: true,
-      style: null,
-    };
-  }
+			if (this.useKirbytext) {
+				const textAttr = hasText ? ` text: ${text}` : "";
+				this.editor.insert(`(email: ${email}${textAttr})`);
+			} else if (hasText) {
+				this.editor.insert(`[${text}](mailto:${email})`);
+			} else {
+				this.editor.insert(`<${email}>`);
+			}
+		} else {
+			if (this.useKirbytext) {
+				const textAttr = hasText ? ` text: ${text}` : "";
+				const targetAttr = target ? " target: _blank" : "";
+				this.editor.insert(`(link: ${href}${textAttr}${targetAttr})`);
+			} else if (hasText) {
+				this.editor.insert(`[${text}](${href})`);
+			} else {
+				this.editor.insert(`<${href}>`);
+			}
+		}
+	}
 
-  get useKirbytext() {
-    return ([null, 'kirbytext'].includes(this.options.style) && this.input.kirbytext);
-  }
+	linkType(value) {
+		if (typeof value !== "string") {
+			return "custom";
+		}
 
-  get dialog() {
-    return "k-markdown-link-dialog";
-  }
+		if (/^(http|https):\/\//.test(value)) {
+			return "url";
+		}
 
-  keys() {
-    return [
-      {
-        key: "Mod-k",
-        run: () => this.editor.emit("dialog", this),
-      }
-    ];
-  }
+		if (value.startsWith("page://") || value.startsWith("/@/page/")) {
+			return "page";
+		}
 
-  get name() {
-    return "link";
-  }
+		if (value.startsWith("file://") || value.startsWith("/@/file/")) {
+			return "file";
+		}
+
+		if (value.startsWith("tel:")) {
+			return "tel";
+		}
+
+		if (value.startsWith("email:")) {
+			return "email";
+		}
+
+		if (value.startsWith("#")) {
+			return "#";
+		}
+
+		return "custom";
+	}
+
+	configure(options) {
+		if (typeof options === "string") {
+			options = { style: options };
+		}
+
+		Button.prototype.configure.call(this, options);
+
+		if (!["markdown", "kirbytext", null].includes(this.options.style)) {
+			throw "Link style must be either `markdown`, `kirbytext` or `null`.";
+		}
+	}
+
+	get defaults() {
+		return {
+			blank: true,
+			style: null
+		};
+	}
+
+	get useKirbytext() {
+		return (
+			[null, "kirbytext"].includes(this.options.style) && this.input.kirbytext
+		);
+	}
+
+	keys() {
+		return [
+			{
+				key: "Mod-k",
+				run: () => this.openDialog()
+			}
+		];
+	}
+
+	get name() {
+		return "link";
+	}
 }
