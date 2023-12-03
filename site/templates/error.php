631f9b7b7f71a7d1fe3b27828c8594ae766a2a2f
Joseph Tatum <josephtatum@gmail.com>
2023-12-03 11:00:23 -0800

    updating error page

---
+++ site/templates/error.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= $site->title() ?></title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <?= css('assets/css/styles.css') ?>
+    <?= css('assets/css/blog-detail.css') ?>
+    <?= css('assets/css/bapse.css') ?>
+  </head>
+  <body>
+    <main class="h-screen flex items-center justify-center">
+      <p>Whoops! I don't know where you're trying to go. <a href="/"> Go back to safety?</a></p>
+    </main>
+  </body>
+</html>
